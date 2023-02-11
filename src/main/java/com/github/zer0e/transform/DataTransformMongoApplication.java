6567a836e412d5b73facbad66bd6eac0c2a2d72a
zer0e <zcluslloye@gmail.com>
2023-02-11 21:47:17 +0800

    new spring project

---
+++ src/main/java/com/github/zer0e/transform/DataTransformMongoApplication.java
@@ -0,0 +1,13 @@
+package com.github.zer0e.transform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTransformMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataTransformMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/zer0e/transform/DataTransformMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zer0e.transform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTransformMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
